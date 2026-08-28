47dfad18a13fece2a16b1d1b882490470c7e6d55
Kirti Kishore <154396007+kiki101robo@users.noreply.github.com>
2026-08-28 17:03:31 -0400

    Add files via upload

---
+++ 9_adc_dma_timer_trigger/Debug/Src/adc_dma.d
@@ -0,0 +1,18 @@
+Src/adc_dma.o: ../Src/adc_dma.c ../Inc/adc_dma.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/adc_dma.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/9_adc_dma_timer_trigger/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ 9_adc_dma_timer_trigger/Src/main.c
@@ -0,0 +1,47 @@
+#include "stm32f4xx.h"
+#include <stdio.h>
+#include "dma.h"
+#include "uart.h"
+#include "adc_dma.h"
+
+#define BUFFER_SIZE		5
+
+extern uint16_t adc_raw_data_a[NUM_OF_SAMPLES];
+
+volatile uint8_t g_transfer_cmplt;
+int main(void)
+{
+	g_transfer_cmplt = 0;
+	uart2_tx_init();
+	adc_tim_dma_init();
+
+	while(1)
+	{
+		if(g_transfer_cmplt)
+		{
+			g_transfer_cmplt = 0;
+			for(int i=0; i<10; i++)
+			{
+				printf("Sample number [%d] = %d \r\n ", i, adc_raw_data_a[i]);
+			}
+			for(int i = 0; i<9999; i++){}
+		}
+	}
+
+}
+
+void DMA2_Stream0_IRQHandler(void)
+{
+
+	/*Check if transfer complete interrupt occurred*/
+	if((DMA2->LISR) & LISR_TCIF0)
+	{
+		g_transfer_cmplt = 1;
+
+		/*Clear flag*/
+		DMA2->LIFCR |=LIFCR_CTCIF0;
+	}
+
+
+}
+
