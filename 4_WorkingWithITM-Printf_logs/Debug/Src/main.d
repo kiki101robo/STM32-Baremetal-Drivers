77ea47698ebe598a762f8764efad70632b35fcaa
Kirti Kishore <154396007+kiki101robo@users.noreply.github.com>
2026-08-28 16:51:31 -0400

    Add files via upload

---
+++ 4_WorkingWithITM-Printf_logs/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/swo.h
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/4_WorkingWithITM-Printf_logs/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/swo.h:
