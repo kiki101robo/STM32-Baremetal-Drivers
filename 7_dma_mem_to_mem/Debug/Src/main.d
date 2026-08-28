7a1e5c0fa4b58505c4500a7e3b299246fb5566ce
Kirti Kishore <154396007+kiki101robo@users.noreply.github.com>
2026-08-28 17:01:09 -0400

    Add files via upload

---
+++ 7_dma_mem_to_mem/Debug/Src/main.d
@@ -0,0 +1,20 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/dma.h ../Inc/uart.h
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/kisho/Documents/STM32_Advanced_Baremetal/7_dma_mem_to_mem/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/dma.h:
+../Inc/uart.h:
